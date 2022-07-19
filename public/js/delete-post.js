a03db5d881cfcff031b7722fbccf231b5305f8d9
Plavala <oscar.cdbs@gmail.com>
2022-07-19 17:14:04 -0400

    created the add and delete post option

---
+++ public/js/delete-post.js
@@ -0,0 +1,20 @@
+// This will get rid of a post (I hope)
+async function deleteFormHandler(event) {
+    event.preventDefault();
+
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch(`/api/posts/${post_id}`, {
+        method: 'DELETE'
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
